4c12381a5ad1f01274cb7de9b635d6bdd29808d9
Adil <kokenadil3@gmail.com>
2025-02-16 18:12:15 -0500

    tests added to the library, setup file written for package

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="recursion2loop",
+    version="0.1.0",
+    packages=find_packages(include=["recursion2loop"]),
+    description="conversion of recursive functions to iterative implementations. Optimize your Python code by transforming stack-heavy recursion into efficient loops.",
+    author='Adil Köken',
+    install_requires=[],
+    setup_requires=['pytest-runner'],
+    tests_require=['pytest'],
+    test_suite='tests',
+)
+
