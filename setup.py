87916a51dd65612d030f03f475109f42474523f2
Adil <kokenadil3@gmail.com>
2025-02-16 18:24:17 -0500

    changed the init file for a more clean and simple import style (tree traversals are imported as a part of the package, no need to specify single file)

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="recursion2loop",
-    version="0.1.0",
+    version="0.1.1",
     packages=find_packages(include=["recursion2loop"]),
     description="conversion of recursive functions to iterative implementations. Optimize your Python code by transforming stack-heavy recursion into efficient loops.",
     author='Adil Köken',
